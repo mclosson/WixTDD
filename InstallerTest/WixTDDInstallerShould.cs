101822b87886914a0b1b9e5e28ce74f55555641a
unknown <usd25851@USDMLBNOD1NB294.code1.emi.philips.com>
2012-04-10 04:51:23 -0400

    Initial commit of WixTDD demo projects

---
+++ InstallerTest/WixTDDInstallerShould.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace InstallerTest
+{
+    [TestClass]
+    public class WixTDDInstallerShould
+    {
+        
+        [AssemblyInitialize]
+        public static void Startup(TestContext testContext)
+        {
+            String msiFile = GetFullPathToMsiFile();
+            Process.Start("MSIEXEC", @"/i " + msiFile + " /passive").WaitForExit();
+        }
+
+        private static String GetFullPathToMsiFile()
+        {
+            String currentPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            String msiPath = Regex.Split(currentPath, "WixTDD").First() + @"WixTDD\WixTDDInstaller\bin\Debug\";
+            String msiFile = msiPath + "WixTDDInstaller.msi";
+            return msiFile;
+        }
+
+        [AssemblyCleanup]
+        public static void Cleanup()
+        {
+            String msiFile = GetFullPathToMsiFile();
+            Process.Start("MSIEXEC", @"/x " + msiFile + " /passive").WaitForExit();
+        }
+
+        [TestMethod]
+        public void CopiesWixTddExeToInstallFolder()
+        {
+            String fileName = @"C:\Program Files (x86)\WixTDDInstaller\WixTDD.exe";
+            String errorMessage = fileName + " was not found";
+            Assert.IsTrue(File.Exists(fileName), errorMessage);
+        }
+
+        [TestMethod]
+        public void CopiesWixTddPdbToInstallFolder()
+        {
+            String fileName = @"C:\Program Files (x86)\WixTDDInstaller\WixTDD.pdb";
+            String errorMessage = fileName + " was not found";
+            Assert.IsTrue(File.Exists(fileName), errorMessage);
+        }
+    }
+}
---
+++ WixTDD/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WixTDD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Installer development using WiX and TDD");
+        }
+    }
+}
